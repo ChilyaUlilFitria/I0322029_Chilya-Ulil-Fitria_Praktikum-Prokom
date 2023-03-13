f8c2c5498999456535f0a7dab0c5d42fb123caed
ChilyaUlilFitria <chilyaulilfitria@gmail.com>
2023-03-13 22:42:51 +0700

    tugas1

--- exercise-01.py
+++ exercise-01.py
@@ -1,7 +1,9 @@
-print("+"*55)
+print("-"*55)
 print("\n"+"Nama : Chilya Ulil Fitria")
 print("NIM : I0322029"+"\n")
-print("+"*55 + "\n")
+print("-"*55 + "\n")
 
 print("""
-    Saya Chilya Ulil Fitria dengan NIM I0322029. Lahir di kota asal makanan lunpia, yaitu Kota Semarang. Semarang merupakan kota dengan wisata yang banyak, meskipun banyak kota ini mempunyai lingkungan yang bersih. Tak heran jika Semarang dijuluki kota wisata paling bersih se-Asia Tenggara.""")
+    Saya Chilya Ulil Fitria dengan NIM I0322029. Lahir di kota dengan lunpia sebagai makanan khasnya, yaitu Kota Semarang. Semarang merupakan kota dengan wisata yang banyak, meskipun banyak kota ini mempunyai lingkungan yang bersih. Tak heran jika Semarang dijuluki kota wisata paling bersih se-Asia Tenggara. 
+    Di bangku SMP dan SMA saya menempuh pendidikan di kota ini, tetapi saat ini saya berkuliah sekaligus tinggal di Kota Surakarta. Surakarta memiliki budaya yang tidak jauh berbeda dengan Semarang. Ketidaksukaan saya terhadap Kota Surakarta adalah bau sampahnya yang sering kali tercium walaupun tempat itu tidak terlalu kotor. Saya berharap pemerintah bisa mengatasi masalah tersebut, karena sayang sekali jika kota yang indah dan ramah ini harus mempunyai bau yang tidak sedap.
+    Di Surakarta saya menempuh pendidikan di Universitas Sebelas Maret di jurusan Teknik Industri. Saya memilih Teknik Industri karena terkenal dengan prospek kerjanya yang luas. Setelah lulus dari Teknik Industri tentu saja saya berharap dapat memiliki pekerjaan yang cocok untuk saya dan mampu memanfaatkan potensi yang saya miliki setelah mendapatkan ilmu dari jurusan tersebut.""")
